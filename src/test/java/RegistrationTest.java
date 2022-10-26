d4712ed8dbfd85ae80f378ac56edf4567a54bfef
Vurhis1 <denraimond@gmail.ru>
2022-10-26 22:51:58 +0300

    change test 2

--- src/test/java/RegistrationTest.java
+++ src/test/java/RegistrationTest.java
@@ -1,6 +1,14 @@
 import com.codeborne.selenide.Condition;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -10,23 +18,51 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class RegistrationTest {
 
-    String generateData (int currentDate) {
-        return LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    public class CallBackTest {
+        private WebDriver driver;
+
+        @BeforeAll
+        void setupAll() {
+            WebDriverManager.chromedriver().setup();
+        }
+
+        @BeforeEach
+        public void setUp() {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--headless");
+            driver = new ChromeDriver(options);
+        }
+
+
+        @AfterEach
+        void tearsDown() {
+            driver.quit();
+            driver = null;
+        }
     }
 
-    @Test
-    void shouldRegisterByAccountNumberDOMModification() {
-        String date = generateData(3);
-        open("http://localhost:9999/");
-        $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='name'] input").setValue("Агафонов Илья");
-        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT,Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").setValue("10.11.2022");
-        $("[data-test-id='phone'] input").setValue("+79251111111");
-        $("[data-test-id='agreement']").click();
-        $$("button").find(Condition.exactText("Забронировать")).click();
-        $("[data-test-id='notification']").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        String generateData(int currentDate) {
+            return LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        }
+
+
+        @Test
+        void shouldRegisterByAccountNumberDOMModification() {
+            String date = generateData(3);
+            open("http://localhost:9999/");
+            $("[data-test-id='city'] input").setValue("Москва");
+            $("[data-test-id='name'] input").setValue("Агафонов Илья");
+            $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+            $("[data-test-id='date'] input").setValue("10.11.2022");
+            $("[data-test-id='phone'] input").setValue("+79251111111");
+            $("[data-test-id='agreement']").click();
+            $$("button").find(Condition.exactText("Забронировать")).click();
+            $("[data-test-id='notification']").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        }
     }
-}
+
+
 
 
